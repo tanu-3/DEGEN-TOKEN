07c1bac23dc9cabdc9af0cb6f6c7ec2c098436ba
Tanu Pal <tanup0010@gmail.com>
2024-06-21 19:13:43 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,38 +1,82 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
+pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenToken is ERC20,  {
+contract DegenToken is ERC20, Ownable {
 
-    constructor() ERC20("Degen", "DGN") {}
-
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
-    }
-        function decimals() override public pure returns (uint8){
-            return 0;
-    }
-    function getBalance() external view returns (uint256){
-        return this.balance0f(msg.sender);
-    }
-    function transferTokens(address _receiver, uint256 _value)external{
-        require(balance0f(msg.sender) >=_value, "You do not have enough Degen tokens");
-        approve(msg.sender, _value);
-        transferFrom(msg.sender, _receiver, _value);
-    }
-    function burnTokens(uint 256 _value) external{
-        require(balance0f(msg.sender) >=_value, "You do not have enough Degen Tokens");
-         _burn(msg.sender, _value);
-    }
-    function redeemTokens(uint256 _value) external{
-        address storeAddress = 0xstoreAddress;
-        require(balance0f(msg.sender) >=_value,"You do not have enough tokens to redeem");
-        emit redeemTokens(msg.sender, storeAddress,_value);
-    }
+    struct Tokencards {
+        uint A;
+        uint B;
+        uint C;
+        }
+        
+        address public storeAddress;
     
-    function checkBalance(address account) external view returns (uint256){
-        return balanceOf(account);
-    }
+    mapping(address => Tokencards) public userCards;
+    
+    event TokensRedeemed(address indexed redeemer, address indexed storeAddress, uint256 amount);
+    
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+    
+        storeAddress = address(0x1234567890AbcdEF1234567890aBcdef12345678);
+        } // Default store address}
+    
+    function setStoreAddress(address _storeAddress) external onlyOwner {
+        storeAddress = _storeAddress;
+        }
+    
+    
+    function mint(address to, uint256 amount) public onlyOwner {
+    require(to != address(0), "Cannot mint to zero address");
+    require(amount > 0, "Amount must be greater than zero");
+    _mint(to, amount);
 }
+    function decimals() public pure override returns (uint8) { 
+        return 0;
+        }
+
+    function getBalance() external view returns (uint256) {
+        return balanceOf(msg.sender);
+        }
+
+    function transferTokens(address _receiver, uint256 _amount) external {
+
+        require(balanceOf(msg.sender) >= _amount, "You do not have enough Degen tokens");
+        transfer(_receiver, _amount);
+        }
+
+    function burnTokens(uint256 _value) external {
+
+        require(balanceOf(msg.sender) >= _value, "You do not have enough Degen Tokens");
+        _burn(msg.sender, _value);
+        }
+
+    function redeemTokens(uint256 _value, uint input) external returns (string memory) {
+        
+        Tokencards storage userCard = userCards[msg.sender];
+   
+   if (input == 1) {
+    userCard.A++;
+    } else if (input == 2) {
+        userCard.B++;
+        } else if (input == 3) {  
+            userCard.C++;
+        } else {
+             return "Unknown token card";
+             }
+        require(balanceOf(msg.sender) >= _value, "You do not have enough tokens to redeem");
+        
+        _burn(msg.sender, _value);
+
+        require(storeAddress != address(0), "Store address is not set");
+
+        emit TokensRedeemed(msg.sender, storeAddress, _value);
+
+        return "Tokens redeemed successfully";
+        }
+
+function checkBalance(address account) external view returns (uint256) {
+        return balanceOf(account);
+        }
+        }
